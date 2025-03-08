7c6fe3eda7956f0dd4ec49cc284b277d55a6ec59
Vincent Roberson <vincent@varmail.org>
2025-03-08 15:36:21 -0800

    added an else just to try something 	modified:   peer.c

--- peer.c
+++ peer.c
@@ -148,13 +148,14 @@ void publish(int *s, char *buf) {
 		if (strcmp(dir->d_name, ".") == 0 || strcmp(dir->d_name, "..") == 0) {
         	continue;  // Skip these special directory entries
     	}
-		
-		count++;
-		// Where I go this
-		// https://chatgpt.com/share/67c8abd2-5d50-800a-853f-55de0a46d0c1
-		strcpy(&buf[fileNameOffset], dir->d_name);
-		// dir->d_name is the current filename
-		fileNameOffset += strlen(dir->d_name) + 1;
+		else {
+			count++;
+			// Where I go this
+			// https://chatgpt.com/share/67c8abd2-5d50-800a-853f-55de0a46d0c1
+			strcpy(&buf[fileNameOffset], dir->d_name);
+			// dir->d_name is the current filename
+			fileNameOffset += strlen(dir->d_name) + 1;
+		}
 	}
 	closedir(d);
 
